5685ed00b488df4cda36ed07290507b80f89d753
Ahiles <ahilespelid@yandex.ru>
2022-09-30 11:55:30 +0000

    zip and sql all new atribute files gitfls

--- core/model/aws/lib/cachecore/_sql/sqlite3.sql
+++ core/model/aws/lib/cachecore/_sql/sqlite3.sql
@@ -1,2 +1,3 @@
-CREATE TABLE cache (id TEXT, expires NUMERIC, data BLOB);
-CREATE UNIQUE INDEX idx ON cache(id ASC);
+version https://git-lfs.github.com/spec/v1
+oid sha256:96f0a6ba5eaa2ee9e750682103e763ad662a66250eeb3ce8df6a8eff51908bd3
+size 99
